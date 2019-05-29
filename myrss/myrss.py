2b49a224b6cd0bf736584492f68e5ae03e08c645
Antonio Cuni <anto.cuni@gmail.com>
2019-05-29 11:56:07 +0200

    refactor to make it easier to extend

--- myrss/myrss.py
+++ myrss/myrss.py
@@ -3,40 +3,60 @@ import urlparse
 from lxml import etree
 import requests
 
-GAZZETTA_RSS = 'https://www.gazzetta.it/rss/home.xml'
+class AbstractFeed(object):
 
+    URL = None
+
+    def fetch_and_filter(self):
+        resp = requests.get(self.URL)
+        resp.raise_for_status()
+        headers = []
+        for header in ('Content-Type', 'Date', 'Expires', 'Cache-Control'):
+            headers.append((header, resp.headers[header]))
+        return headers, self.filter(resp.text.encode('utf-8'))
+
+    def filter(self, content):
+        raise NotImplementedError
+
+class Gazzetta(AbstractFeed):
+
+    URL = 'https://www.gazzetta.it/rss/home.xml'
+
+    def filter(self, content):
+        rss = etree.fromstring(content)
+        for item in rss.xpath("//item"):
+            link = item.find('link')
+            if link is None:
+                continue
+            if link.text.startswith('http'):
+                item.getparent().remove(item)
+        return etree.tostring(rss, pretty_print=True, xml_declaration=True,
+                              encoding='UTF-8')
+
+
+# WSGI-compatible entry point
 def application(environ, start_response):
     uri = util.request_uri(environ)
     path = urlparse.urlsplit(uri).path
     if path.startswith('/gazzetta'):
-        return gazzetta(start_response)
-    start_response('404 Not Found', [])
-    return ['Not found: %s' % path]
-
-def gazzetta(start_response):
-    resp = requests.get(GAZZETTA_RSS)
-    resp.raise_for_status()
-    headers = []
-    for header in ('Content-Type', 'Date', 'Expires', 'Cache-Control'):
-        headers.append((header, resp.headers[header]))
+        feed = Gazzetta()
+    else:
+        start_response('404 Not Found', [])
+        return ['Not found: %s' % path]
+    #
+    headers, content = feed.fetch_and_filter()
     start_response('200 OK', headers)
-    return [filter_gazzetta_rss(resp.text.encode('utf-8'))]
-
-def filter_gazzetta_rss(content):
-    rss = etree.fromstring(content)
-    for item in rss.xpath("//item"):
-        link = item.find('link')
-        if link is None:
-            continue
-        if link.text.startswith('http'):
-            item.getparent().remove(item)
-    return etree.tostring(rss, pretty_print=True, xml_declaration=True,
-                          encoding='UTF-8')
+    return [content]
 
 
+# only useful for debugging/development
 def main():
-    resp = requests.get(GAZZETTA_RSS)
-    print filter_gazzetta_rss(resp.text.encode('utf-8'))
+    feed = Gazzetta()
+    headers, content = feed.fetch_and_filter()
+    for key, value in headers:
+        print '%s: %s' % (key, value)
+    print
+    print content
 
 if __name__ == '__main__':
     main()
